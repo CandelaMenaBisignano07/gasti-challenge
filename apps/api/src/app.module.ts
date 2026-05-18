c5eb853262a7a8a6d2da948597d85cdb73d7b466
CandelaMenaBisignano07 <candelamenabisignano07@gmail.com>
2026-05-17 22:21:55 -0300

    feat(api): wire feature modules, CORS and global error filter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { SharedModule } from './shared/shared.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CategorizationModule } from './categorization/categorization.module';
+import { SpendingModule } from './spending/spending.module';
+import { InsightsModule } from './insights/insights.module';
+import { BudgetsModule } from './budgets/budgets.module';
+import { IncomeModule } from './income/income.module';
+import { GoalsModule } from './goals/goals.module';
 
 @Module({
+  imports: [
+    SharedModule,
+    TransactionsModule,
+    CategorizationModule,
+    SpendingModule,
+    InsightsModule,
+    BudgetsModule,
+    IncomeModule,
+    GoalsModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
